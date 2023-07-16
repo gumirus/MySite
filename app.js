82d1d5691c7dd1407e941a35b8d5221673881ef4
Ruslan Gumirov <gumirus@gmail.com>
2023-07-16 13:41:07 +0500

    fix: add if else and fix:correction

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Дата рождения
 const birthDate = "1982-11-20";
 
-// Дата начала работы
+// Дата начала учёбы
 const startWork = "2022-07-27";
 
 // Получаем элементы
@@ -10,11 +10,8 @@ const workEl = document.getElementById("start-work");
 
 // Функция обновления дат
 function updateDates() {
-  // Текущая дата
-  const now = new Date();
-
-  // Возраст
-  const birth = new Date(birthDate);
+  const now = new Date(); // Текущая дата
+  const birth = new Date(birthDate); // Возраст
   let age = now.getFullYear() - birth.getFullYear();
   const monthDiff = now.getMonth() - birth.getMonth();
 
@@ -22,13 +19,25 @@ function updateDates() {
     age--;
   }
 
-  // Дней работы
+  // Дни практической работы
   const start = new Date(startWork);
   const workDays = Math.round((now - start) / (1000 * 60 * 60 * 24));
+  // использовать условный оператор if, else, else if для правописания день, дня, дней
+  if (workDays % 10 == 1 && workDays != 11) {
+    word = "день";
+  } else if (
+    workDays % 10 >= 2 &&
+    workDays % 10 <= 4 &&
+    !(workDays >= 12 && workDays <= 14)
+  ) {
+    word = "дня";
+  } else {
+    word = "дней";
+  }
 
   // Формируем текст
   const birthText = "Мне " + age + " лет";
-  const workText = "Дни практической учёбы: " + workDays + " дней";
+  const workText = "Практикуюсь: " + workDays + " " + word;
 
   // Выводим в элементы
   birthEl.innerText = birthText;
